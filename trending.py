aea199fb6f7163a5e743b0996e3593b44bbd4c80
cperron-coder <69486313+cperron-coder@users.noreply.github.com>
2021-11-20 21:55:57 -0700

    Update trending.py

--- trending.py
+++ trending.py
@@ -2,5 +2,10 @@ import requests
 def printtrend():
     response = requests.get("https://api.coingecko.com/api/v3/search/trending")
     data_dict = response.json()
+    print("ALL PRICES IN CAD: ")
+    print(" ")
+    print("The top 7 trending coins are: ")
+    print(" ")
     for x in range(7):
         print(data_dict['coins'][x]['item']['id'], "(" + data_dict['coins'][x]['item']['symbol'] + ")")
+    print(" ")
