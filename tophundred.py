c33b6d036444f6451492617700f196658790e29d
cperron-coder <69486313+cperron-coder@users.noreply.github.com>
2021-11-20 21:55:28 -0700

    Update tophundred.py

--- tophundred.py
+++ tophundred.py
@@ -6,6 +6,8 @@ def print100():
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1"
         "&sparkline=false")
     data_dict = response.json()
+    print(" ")
+    print("The price for the top 100 coins are: ")
+    print(" ")
     for x in range(100):
         print(data_dict[x]['id'], data_dict[x]['current_price'])
-
